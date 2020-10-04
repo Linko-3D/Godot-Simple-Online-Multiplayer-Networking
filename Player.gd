ded35742a823387f693ef38ad8fa0f475aff5271
Linko-3D <dbekhouc@gmail.com>
2020-10-04 18:18:41 +0200

    tweak

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var direction = Vector2()
+
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_on_network_peer_connected")
 	yield(get_tree(), "idle_frame") # Wait one frame before checking if we are the master of this node
@@ -7,8 +9,11 @@ func _ready():
 
 func _physics_process(delta):
 	if is_network_master():
-		position.x += (-Input.get_action_strength("ui_left") + Input.get_action_strength("ui_right")) * 5
-		if position != Vector2(): # If we are moving the character send rpc call
+		direction.x = -Input.get_action_strength("ui_left") + Input.get_action_strength("ui_right")
+		direction.y = -Input.get_action_strength("ui_up") + Input.get_action_strength("ui_down")
+		direction.normalized()
+		move_and_slide(direction * 500)
+		if direction != Vector2(): # If we are moving the character sends rpc call
 			rpc_unreliable("transform_data", transform)
 		
 		if Input.is_action_just_pressed("ui_accept"):
