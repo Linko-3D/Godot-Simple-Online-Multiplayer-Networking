0445ad15023c1e16a35aac67692c3c11abaf5609
Linko-3D <dbekhouc@gmail.com>
2020-10-04 17:58:18 +0200

    tweak

--- Player.gd
+++ Player.gd
@@ -10,13 +10,12 @@ func _physics_process(delta):
 		position.x += (-Input.get_action_strength("ui_left") + Input.get_action_strength("ui_right")) * 5
 		rpc_unreliable("transform_data", transform)
 		if Input.is_action_just_pressed("ui_accept"):
-			visible = !visible
-			rpc("visibility", visible)
+			rpc("visibility", !visible)
 
 remote func transform_data(data):
 	transform = data
 
-remote func visibility(data):
+remotesync func visibility(data):
 	visible = data
 
 func _on_network_peer_connected(id):
