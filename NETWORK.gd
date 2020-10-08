0c484dd16d78b90e98883dd3b6afe56623333cfc
Linko-3D <dbekhouc@gmail.com>
2020-10-08 20:47:58 +0200

    tweak

--- NETWORK.gd
+++ NETWORK.gd
@@ -1,11 +1,13 @@
 extends Node
 
+var lobby = preload("res://Lobby.tscn").instance()
 var map = preload("res://Map.tscn").instance()
 
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_on_network_peer_connected")
 	get_tree().connect("network_peer_disconnected", self, "_on_network_peer_disconnected")
 	get_tree().connect("connected_to_server", self, "_on_connected_to_server")
+	get_tree().connect("server_disconnected", self, "_on_server_disconnected")
 
 func create_server():
 	var peer = NetworkedMultiplayerENet.new()
@@ -39,7 +41,13 @@ func _on_network_peer_connected(id):
 		spawn_player(id)
 
 func _on_network_peer_disconnected(id):
-	get_tree().get_root().find_node(str(id), true, false).queue_free()
+	if id != 1:
+		get_tree().get_root().find_node(str(id), true, false).queue_free()
 
 func _on_connected_to_server():
 	load_game()
+
+func _on_server_disconnected():
+	get_tree().get_root().add_child(lobby)
+	get_tree().get_root().get_node("Map").queue_free()
+	get_tree().set_network_peer(null)
