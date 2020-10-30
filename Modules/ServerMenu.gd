824a054f1b012de2591140ff1b9ac3c1d981db12
Linko-3D <dbekhouc@gmail.com>
2020-10-30 17:20:31 +0100

    tweak

--- Modules/ServerMenu.gd
+++ Modules/ServerMenu.gd
@@ -1,8 +1,20 @@
 extends Control
 
-func _process(delta):
-	var spawn_node = get_tree().get_root().find_node("Spawn", true, false)
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_on_network_peer_connected")
+	get_tree().connect("network_peer_disconnected", self, "_on_network_peer_disconnected")
+
+func _on_network_peer_connected(id):
+	update()
+
+func _on_network_peer_disconnected(id):
+	update()
+
+func update():
+	yield(get_tree(), "idle_frame")
 	
+	var spawn_node = get_tree().get_root().find_node("Spawn", true, false)
+
 	for child in $ConnectedList.get_children():
 		child.queue_free()
 
@@ -10,6 +22,13 @@ func _process(delta):
 		display_player(child.player_name)
 
 func display_player(player_name):
+	var HBox = HBoxContainer.new()
+	$ConnectedList.add_child(HBox)
+
+	var button = Button.new()
+	button.text = "Kick"
+	HBox.add_child(button)
+
 	var player_connected = Label.new()
 	player_connected.text = "- " + player_name
-	$ConnectedList.add_child(player_connected)
+	HBox.add_child(player_connected)
