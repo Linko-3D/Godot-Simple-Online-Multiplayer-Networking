a7d07a30973073187c5a3deba90c488efbd95f44
Linko-3D <dbekhouc@gmail.com>
2021-06-21 16:28:51 +0200

    tweak

--- networking/Chat.gd
+++ networking/Chat.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var max_messages = 5
+var max_messages = 10
 
 var can_use = true
 
@@ -16,15 +16,14 @@ func _ready():
 	get_tree().connect("screen_resized", self, "_on_screen_resized")
 	
 	$Message.hide()
-	
 	update_position()
 	
 	yield(get_tree(), "idle_frame")
 	
 	if get_tree().is_network_server():
-		send_message("-- Server is running --", Color(1, 1, 0))
+		rpc("send_message_online", "-- Server is running --", Color(1, 1, 0))
 	else:
-		send_message("-- " + Network.username + " has joined the game --", Color(1, 1, 0))
+		rpc("send_message_online", "-- " + Network.username + " has joined the game --", Color(1, 1, 0))
 
 func _input(event):
 	if Input.is_key_pressed(KEY_ENTER):
@@ -33,7 +32,7 @@ func _input(event):
 			
 			if $Message.visible:
 				if $Message/TypedMessage.text != "":
-					send_message(Network.username + ": " + $Message/TypedMessage.text, Color(1, 1, 1))
+					rpc("send_message_online", Network.username + ": " + $Message/TypedMessage.text, Color(1, 1, 1))
 				$Message.visible = false
 				$Message/TypedMessage.clear()
 				$MessagesFadeOutTimer.start()
@@ -44,9 +43,6 @@ func _input(event):
 	else:
 		can_use = true
 
-func send_message(data, color):
-	rpc("send_message_online", data, color)
-
 remotesync func send_message_online(message, color):
 	$ChatBox.show()
 	
@@ -54,9 +50,9 @@ remotesync func send_message_online(message, color):
 		$MessagesFadeOutTimer.start()
 	
 	var display_message = Label.new()
+	display_message.text = message
 	display_message.modulate = color
 	$ChatBox.add_child(display_message)
-	display_message.text = message
 	
 	# Limit the messages that are displayed
 	if $ChatBox.get_child_count() > max_messages:
