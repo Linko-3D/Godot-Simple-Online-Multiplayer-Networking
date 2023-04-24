0c123bfc3c7a3451681311dbbd405407c2806f37
Linko-3D <dbekhouc@gmail.com>
2023-04-24 13:50:30 +0200

    tweak

---
+++ modules/chat.gd
@@ -0,0 +1,42 @@
+extends Control
+
+var enter_key_pressed = false
+
+func _ready():
+	%SendMessage.hide()
+	%SendMessage.position.y = get_viewport().size.y - (get_viewport().size.y / 6)
+
+func _input(event):
+	if Input.is_key_pressed(KEY_ENTER) or Input.is_key_pressed(KEY_KP_ENTER):
+		if not enter_key_pressed:
+			enter_key_pressed = true
+			%SendMessage.visible = !%SendMessage.visible
+			if not %SendMessage.visible:
+				if not %TypedMessage.text == "":
+					send_message.rpc(%TypedMessage.text)
+				%TypedMessage.text = ""
+				%ChatBoxDisapearsTimer.start()
+			else:
+				%DisplayedMessage.show()
+	else:
+		enter_key_pressed = false
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		%SendMessage.hide()
+		%TypedMessage.text = ""
+		%ChatBoxDisapearsTimer.start()
+
+@rpc("call_local", "any_peer")
+func send_message(message):
+	%DisplayedMessage.show()
+	var label_message = Label.new()
+	label_message.text = " " + message
+	%DisplayedMessage.add_child(label_message)
+
+	if %DisplayedMessage.get_child_count() > 7:
+		%DisplayedMessage.get_child(0).queue_free()
+
+	%ChatBoxDisapearsTimer.start()
+
+func _on_chat_box_disapears_timer_timeout():
+	%DisplayedMessage.hide()
