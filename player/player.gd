8a4834c215cd3a8fbbe4d25219a4844d016e1067
Linko-3D <dbekhouc@gmail.com>
2023-06-12 09:54:39 +0200

    tweak

--- player/player.gd
+++ player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 func _ready():
 	set_multiplayer_authority(name.to_int())
-	$Authority.visible = is_multiplayer_authority()
+	$DisplayAuthority.visible = is_multiplayer_authority()
 
 func _physics_process(delta):
 	if not is_multiplayer_authority(): return
