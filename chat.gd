e595c34776c704c688e0cdbaff5564d31aefb883
Linko-3D <dbekhouc@gmail.com>
2023-09-29 17:23:15 +0200

    tweak

--- chat.gd
+++ chat.gd
@@ -1,6 +1,21 @@
 extends Node
 
+func _ready():
+	%InputBox.hide()
+	%Messages.hide()
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		%InputBox.visible = !%InputBox.visible
+
+		if %InputBox.visible == false:
+			_on_say_button_pressed()
+
+func _on_input_text_text_submitted(new_text):
+	_on_say_button_pressed()
+
 func _on_say_button_pressed():
+	%InputBox.hide()
 	send_message.rpc(%InputText.text, str(multiplayer.get_unique_id()))
 	%InputText.text = ""
 
@@ -9,10 +24,12 @@ func send_message(message, playerName):
 	var label = Label.new()
 	label.text = playerName + ": " + message
 	%DisplayedMessages.add_child(label)
-	
+
 	if %DisplayedMessages.get_child_count() > 7:
 		%DisplayedMessages.get_child(0).queue_free()
-		
 
-func _on_input_text_text_submitted(new_text):
-	_on_say_button_pressed()
+	%Timer.start()
+	%Messages.show()
+
+func _on_timer_timeout():
+	%Messages.hide()
