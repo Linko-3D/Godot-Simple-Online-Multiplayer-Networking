fb25a11c33f0064667f85e8b7fe02135de22bb39
Linko-3D <dbekhouc@gmail.com>
2023-09-29 15:44:32 +0200

    tweak

--- main.gd
+++ main.gd
@@ -6,12 +6,11 @@ extends Node
 func _ready():
 	%Lobby.hide()
 	%IsServer.hide()
-	
 
 # Server
 func _on_host_button_pressed():
-	#upnp_setup()
-	
+	upnp_setup()
+
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(9999)
 	multiplayer.multiplayer_peer = peer
@@ -19,7 +18,6 @@ func _on_host_button_pressed():
 	multiplayer.peer_disconnected.connect(remove_player)
 
 	%IsServer.show()
-	%DisplayPublicIP.text = "Server"
 	load_game()
 
 #  Client - Call this in the `ready()` function and set the public IP address of your server for automatic joining
