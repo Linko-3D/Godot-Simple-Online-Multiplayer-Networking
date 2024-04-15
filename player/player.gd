f80941b6844ea997ca3dd4329b89aa95ee9d2863
Linko-3D <dbekhouc@gmail.com>
2024-04-15 22:42:18 +0200

    tweak

--- player/player.gd
+++ player/player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
-func _ready():
+func _ready() -> void:
 	set_multiplayer_authority(name.to_int())
 	$DisplayAuthority.visible = is_multiplayer_authority()
+	$AudioListener2D.current = is_multiplayer_authority()
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority(): return
 
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 500
