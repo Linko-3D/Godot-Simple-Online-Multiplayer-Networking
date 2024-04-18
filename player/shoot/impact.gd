5f652be1080b9ca1981c9b1912f1bc61766b4221
Linko-3D <dbekhouc@gmail.com>
2024-04-19 01:39:05 +0200

    impact

---
+++ player/shoot/impact.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
--- player/shoot/shoot.gd
+++ player/shoot/shoot.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var impact : PackedScene
+
 var has_authority = false
 
 var weapon_sway_amount = 5
@@ -14,22 +16,27 @@ func _input(event):
 		mouse_relative_x = clamp(event.relative.x, -50, 50)
 		mouse_relative_y = clamp(event.relative.y, -50, 10)
 		
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if not has_authority: return
 
-	print(mouse_relative_x)
 	%Sway.rotation_degrees.z = lerp(%Sway.rotation_degrees.z, -mouse_relative_x / 10.0, weapon_sway_amount * delta)
 	%Sway.rotation_degrees.y = lerp(%Sway.rotation_degrees.y, -mouse_relative_x / 20.0, weapon_sway_amount * delta)
 	%Sway.rotation_degrees.x = lerp(%Sway.rotation_degrees.x, -mouse_relative_y / 10.0, weapon_sway_amount * delta)
 
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) or Input.get_joy_axis(0, 7) >= 0.5:
+		if %FireRateTimer.is_stopped():
+			shoot.rpc()
+			%FireRateTimer.start()
+			
 
-#	Weapon sway
-	#$Position3D/SwitchAndAttack/Bobbing/LookAtLerp/Sway.rotation_degrees.z = lerp($Position3D/SwitchAndAttack/Bobbing/LookAtLerp/Sway.rotation_degrees.z, -mouse_relative_x / 10, weapon_sway_amount * delta)
-	#$Position3D/SwitchAndAttack/Bobbing/LookAtLerp/Sway.rotation_degrees.y = lerp($Position3D/SwitchAndAttack/Bobbing/LookAtLerp/Sway.rotation_degrees.y, mouse_relative_x / 20, weapon_sway_amount * delta)
-	#$Position3D/SwitchAndAttack/Bobbing/LookAtLerp/Sway.rotation_degrees.x = lerp($Position3D/SwitchAndAttack/Bobbing/LookAtLerp/Sway.rotation_degrees.x, -mouse_relative_y / 10, weapon_sway_amount * delta)
+@rpc("call_local")
+func shoot():
+	if not %RayCast3D.is_colliding(): return
+	var a = impact.instantiate()
+	get_tree().get_root().add_child(a)
+	a.position = %RayCast3D.get_collision_point()
+	#%MapInstance.add_child(map.instantiate())
+	
+	#var impact_instance = impact.instance()
+	#get_tree().get_root().add_child(impact_instance)
