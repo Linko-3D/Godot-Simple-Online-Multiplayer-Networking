b21a8699fed8e5d84806dcb247eccfea06b704f9
Linko-3D <dbekhouc@gmail.com>
2024-04-22 00:49:09 +0200

    sway

--- player/shoot/shoot.gd
+++ player/shoot/shoot.gd
@@ -5,8 +5,8 @@ extends Node3D
 var has_authority = false
 
 var weapon_sway_amount = 5
-var mouse_relative_x = 0
-var mouse_relative_y = 0
+var mouse_relative_x = 0.0
+var mouse_relative_y = 0.0
 
 func _input(event):
 	if not has_authority: return
@@ -20,16 +20,25 @@ func _input(event):
 func _process(delta: float) -> void:
 	if not has_authority: return
 
-	%Sway.rotation_degrees.z = lerp(%Sway.rotation_degrees.z, -mouse_relative_x / 10.0, weapon_sway_amount * delta)
-	%Sway.rotation_degrees.y = lerp(%Sway.rotation_degrees.y, -mouse_relative_x / 20.0, weapon_sway_amount * delta)
-	%Sway.rotation_degrees.x = lerp(%Sway.rotation_degrees.x, -mouse_relative_y / 10.0, weapon_sway_amount * delta)
+	#%Sway.rotation_degrees.z = lerp(%Sway.rotation_degrees.z, -mouse_relative_x / 10.0, weapon_sway_amount * delta)
+	#%Sway.rotation_degrees.y = lerp(%Sway.rotation_degrees.y, -mouse_relative_x / 20.0, weapon_sway_amount * delta)
+	#%Sway.rotation_degrees.x = lerp(%Sway.rotation_degrees.x, -mouse_relative_y / 10.0, weapon_sway_amount * delta)
+
+	print(mouse_relative_x)
+	%Sway.position.x = lerp(%Sway.position.x, -mouse_relative_x / 15000.0, 20 * delta)
+	%Sway.position.y = lerp(%Sway.position.y, mouse_relative_y / 15000.0, 20 * delta)
+
 
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) or Input.get_joy_axis(0, 7) >= 0.5:
 		if %FireRateTimer.is_stopped():
 			shoot.rpc()
 			%FireRateTimer.start()
-			
 
+	if Input.is_key_pressed(KEY_R) or Input.is_joy_button_pressed(0, JOY_BUTTON_Y):
+		var tween = get_tree().create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(%Reload, "rotation_degrees:x", 0, 0)
+		tween.tween_property(%Reload, "rotation_degrees:x", 360, 1)
+		#$ReloadTween.interpolate_property(weapon, "rotation_degrees:x", 0, 360, 1, Tween.TRANS_BACK, Tween.EASE_IN_OUT, 0)
 @rpc("call_local")
 func shoot():
 	if not %RayCast3D.is_colliding(): return
