e97ab9cfb7fbfad40f62ccff395cfdfeb4d66e02
Linko-3D <dbekhouc@gmail.com>
2024-11-09 15:37:29 +0100

    tweak

--- player/player.gd
+++ player/player.gd
@@ -35,14 +35,14 @@ func _physics_process(delta: float) -> void:
 
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = jump_velocity
 
-		$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 1.85, 0.1)
+		$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 1.85, 10 * delta)
 
 		if Input.is_action_pressed("crouch"):
 			speed = 1.8
-			$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 1.38, 0.1)
+			$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 1.38, 10 * delta)
 		elif Input.is_action_pressed("walk"):
 			speed = 3
 		else:
@@ -56,14 +56,15 @@ func _physics_process(delta: float) -> void:
 		var input_dir := Input.get_vector("left", "right", "forward", "backward")
 		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
-			velocity.x = direction.x * speed
+			#velocity.x = direction.x * speed
+			velocity.x = move_toward(velocity.x, direction.x * speed, 1)
 			velocity.z = direction.z * speed
 		else:
 			velocity.x = move_toward(velocity.x, 0, speed)
 			velocity.z = move_toward(velocity.z, 0, speed)
 	else:
-		velocity.x = 0
-		velocity.z = 0
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	%Line1.position.y = lerp(%Line1.position.y, -velocity.length() * 22.9, 20 * delta)
 	%Line1.position.y = clamp(%Line1.position.y, -126, 0)
--- spec_camera_3d.gd
+++ spec_camera_3d.gd
@@ -1,1 +1,4 @@
 extends Camera3D
+
+func _ready() -> void:
+	position = get_tree().get_nodes_in_group("spawn_point")[0].position
