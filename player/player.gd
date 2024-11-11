2c376d0ea3dbc50a4bf4e08b0cc70153b8adea7a
Linko-3D <dbekhouc@gmail.com>
2024-11-11 01:42:49 +0100

    tweak

--- player/player.gd
+++ player/player.gd
@@ -55,6 +55,7 @@ func _physics_process(delta: float) -> void:
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir := Input.get_vector("left", "right", "forward", "backward")
 		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		
 		if direction:
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
--- spec_camera_3d.gd
+++ spec_camera_3d.gd
@@ -1,4 +1,8 @@
-extends Node3D
+extends CharacterBody3D
+
+
+var speed = 5.5 * 4
+
 
 func _ready() -> void:
 	position = get_tree().get_nodes_in_group("spawn_point")[0].position
@@ -12,3 +16,18 @@ func _input(event):
 
 		%Camera3D.rotate_x(-event.relative.y / 2000)
 		%Camera3D.rotation.x = clamp( %Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90) )
+
+
+func _process(delta: float) -> void:
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	velocity.y = Input.get_axis("down", "up") * speed
+
+	move_and_slide()
